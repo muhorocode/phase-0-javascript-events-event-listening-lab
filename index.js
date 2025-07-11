03e6ef7534ff6e2832c97feac8ae750ba147f48e
Elijah Kamanga <elijahkamanga1@gmail.com>
2025-07-11 14:09:46 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 function addingEventListener() {
-}
+const input=document.getElementById('button')
+input.addEventListener('click', () => {
+    alert('I have been clicked!')
+})}
+addingEventListener()
